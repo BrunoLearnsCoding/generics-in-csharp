785e2f31b11487b24103ea35badef5b5b1c41cd8
BrunoLearnsCoding <bruno@ilearncoding.com>
2024-02-17 16:44:52 +0330

    Generic delegate were added

--- Generic Linked List/LinkedList.cs
+++ Generic Linked List/LinkedList.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Generic_Linked_List;
 
@@ -6,23 +7,29 @@ public class LinkedList<T> where T : IEquatable<T>
 {
     private Node<T>? _head = null;
 
-    public void Insert(T value) {
+    public void Insert(T value)
+    {
         var node = new Node<T>(value);
         node.Next = _head;
         _head = node;
     }
 
-    public void Delete(T value) {
+    public void Delete(T value)
+    {
         var current = _head;
         Node<T>? prev = null;
-        
-        while (current is not null) {
-            if (current.Value.Equals(value)) {
-                if (prev is not null) {
+
+        while (current is not null)
+        {
+            if (current.Value.Equals(value))
+            {
+                if (prev is not null)
+                {
                     prev.Next = current.Next;
                     current.Next = null;
                 }
-                else {
+                else
+                {
                     _head = current.Next;
                 }
             }
@@ -31,9 +38,35 @@ public class LinkedList<T> where T : IEquatable<T>
         }
     }
 
-    public void Print() {
+    public IEnumerable<T> GetAll()
+    {
         var current = _head;
-        while(current is not null) {
+        while (current is not null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    public IEnumerable<T> GetWhere(Func<T, bool> condition)
+    {
+        var current = _head;
+        while (current is not null)
+        {
+            if (condition(current.Value) == true)
+            {
+                yield return current.Value;
+            }
+            current = current.Next;
+        }
+
+
+    }
+    public void Print()
+    {
+        var current = _head;
+        while (current is not null)
+        {
             Console.WriteLine(current.Value.ToString());
             current = current.Next;
         }
@@ -44,7 +77,8 @@ internal class Node<T>
 {
     private T _value;
     public Node<T>? Next { get; set; } = null;
-    public T Value { 
+    public T Value
+    {
         get => _value;
     }
     public Node(T value)
--- Generic Linked List/Program.cs
+++ Generic Linked List/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         var list = new LinkedList<Person>();
         list.Insert(new Person("1") {FirstName= "Bruno", LastName = "NotTelling"});
+        list.Insert(new Person("6") {FirstName= "John", LastName = "Ulm"});
         list.Insert(new Person("2") {FirstName= "Nill", LastName = "Johnson"});
         list.Insert(new Person("3") {FirstName= "Brad", LastName = "Pitt"});
         list.Insert(new Person("4") {FirstName= "John", LastName = "Nilson"});
@@ -13,6 +14,14 @@ class Program
 
         list.Delete(new Person("1") {FirstName = "Bruno", LastName = "NotTelling"});
         list.Print();
+        foreach (var p in list.GetAll().Take(2)) {
+            System.Console.WriteLine($"Yield: {p.ToString()}");
+        }
+        foreach (var p in list.GetWhere(person => person.FirstName == "John")) {
+            System.Console.WriteLine($"Found by delegate: {p.ToString()}");
+            
+        }
+
 
     }
 }
